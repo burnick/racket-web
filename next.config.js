ebe7b228922112f007f6e48889083e4abf38c3ce
burnick <arnelceledonio@gmail.com>
2023-01-20 11:41:40 +0800

    fix: try experimental

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {},
+  // reactStrictMode: true,
+  // swcMinify: true,
+  experimental: { granularChunks: true },
   compiler: {
     styledComponents: {
       ssr: true,
