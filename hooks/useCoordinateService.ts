b86cf9b67eda60468e9738ae4521656d42f4105c
burnick <arnelceledonio@gmail.com>
2023-01-21 17:26:08 +0800

    fix: upsert

--- hooks/useCoordinateService.ts
+++ hooks/useCoordinateService.ts
@@ -21,62 +21,24 @@ export const CoordinateService = () => {
   };
 
   const UpsertCoordinates = () => {
-    // const pushCoodinates = async (props: LocationProps) => {
-    //   if (
-    //     !props?.uid ||
-    //     !props?.lat ||
-    //     !props?.lng ||
-    //     !props?.radius ||
-    //     props?.radius <= 0
-    //   ) {
-    //     consoleHelper(props);
-    //     throw new Error('missing location props');
-    //   }
-    //   const findLoc = await api.findCoordinates(props?.uid as string);
-    //   if (findLoc.radius !== props.radius || findLoc.lat !== props.lat) {
-    //     return await api.addCoordinates(props);
-    //   }
-    //   return findLoc;
-    // };
-
-    // return useMutation(pushCoodinates);
-    const queryClient = useQueryClient();
-    queryClient.setMutationDefaults(['add-coordinates'], {
-      mutationFn: (data) => api.addCoordinates(data),
-      onMutate: async (variables) => {
-        const { successCb, errorCb } = variables;
-        // cancel queries against this query key
-        // so that if any other component is consuming this data
-        // is not able to get the old data
-        await queryClient.cancelQueries(['get-coordinates']);
-        // get the cached values of 'get-planets'
-        const previousValue: CoordinatesProps | undefined =
-          queryClient.getQueryData(['get-coordinates']);
-
-        // set the cached data with an added object
-        // i.e the new planet posted
-        queryClient.setQueryData(
-          ['get-coordinates'],
-          [previousValue, { ...variables }]
-        );
-
-        // return previousValue here
-        // we will use it in the next section
-        return { successCb, errorCb, previousValue };
-      },
-      onSuccess: (result, _, context) => {
-        if (context.successCb) {
-          context.successCb(result);
-        }
-      },
-      onError: (error, _, context) => {
-        queryClient.setQueryData(['get-coordinates'], context?.previousValue);
-        if (context.errorCb) {
-          context.errorCb(error);
-        }
-      },
-    });
-    return useMutation(['add-coordinates']);
+    const pushCoodinates = async (props: LocationProps) => {
+      if (
+        !props?.uid ||
+        !props?.lat ||
+        !props?.lng ||
+        !props?.radius ||
+        props?.radius <= 0
+      ) {
+        consoleHelper(props);
+        throw new Error('missing location props');
+      }
+      const findLoc = await api.findCoordinates(props?.uid as string);
+      if (findLoc.radius !== props.radius || findLoc.lat !== props.lat) {
+        return await api.addCoordinates(props);
+      }
+      return findLoc;
+    };
+    return useMutation(pushCoodinates);
   };
 
   return { UpsertCoordinates, GetCoordinates };
