a855d15d777fdd5eafc545467e5bdec3e69e76d2
burnick <arnelceledonio@gmail.com>
2023-02-04 08:42:59 +0800

    fix: button cypress test

--- components/ButtonApp.cy.tsx
+++ components/ButtonApp.cy.tsx
@@ -4,6 +4,6 @@ import Button from './Button';
 describe('<App />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Button>My Button</Button>);
+    cy.mount(<Button onClick={() => alert('test')}>My Button</Button>);
   });
 });
