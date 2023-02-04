9053cd0eba1cd5a7fa4081354f0c1771b331e791
burnick <arnelceledonio@gmail.com>
2023-02-04 08:40:47 +0800

    fix: added cypress config

---
+++ components/ButtonApp.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from './Button';
+
+describe('<App />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Button>My Button</Button>);
+  });
+});
